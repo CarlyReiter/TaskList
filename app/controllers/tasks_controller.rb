5768bf607fe65a21edbfc1ac0cee55aeccd6340b
Carly Reiter <yepodacarly@gmail.com>
2018-09-17 14:15:03 -0700

    Completed Baseline section of project, connecting the class Tasks to the internet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,13 @@
+TASKS = [
+  {name: "Mowing the lawn", duration: 25},
+  {name: "Raking the leaves", duration: 30},
+  {name: "Doing the laundry", duration: 10},
+  {name: "Vacuumming", duration: 15},
+  {name: "Cooking dinner", duration: 60},
+]
+
 class TasksController < ApplicationController
   def index
+    @tasks = TASKS
   end
 end
