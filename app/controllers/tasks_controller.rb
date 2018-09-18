17dc8c3abd09a603b866540ae039a91b60bb2a64
Carly Reiter <yepodacarly@gmail.com>
2018-09-18 15:26:08 -0700

    Day 2 Rails activities - creating models, schema, and migrating to tables, creating tasks and updating the controller and view for both.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,19 @@
-TASKS = [
-  {name: "Mowing the lawn", duration: 25},
-  {name: "Raking the leaves", duration: 30},
-  {name: "Doing the laundry", duration: 10},
-  {name: "Vacuumming", duration: 15},
-  {name: "Cooking dinner", duration: 60},
-]
+# TASKS = [
+#   {name: "Mowing the lawn", duration: 25},
+#   {name: "Raking the leaves", duration: 30},
+#   {name: "Doing the laundry", duration: 10},
+#   {name: "Vacuumming", duration: 15},
+#   {name: "Cooking dinner", duration: 60},
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    @task = Task.find_by(id: [task_id])
 
     if @task.nil?
       head :not_found
