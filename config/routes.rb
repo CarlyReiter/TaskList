8c0058077211e68b31a998263f0de6e4d5fd174b
Carly Reiter <yepodacarly@gmail.com>
2018-09-19 15:46:10 -0700

    Updated route.rb with all 7 of the restful routes, in addition to the root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'tasks#index'
+
   get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: 'book'
+
+  get '/tasks/new', to: 'tasks#new', as: 'new_book'
+  post '/tasks', to: 'tasks#create', as: 'books'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
