e0bb1f08412e7ba58299dcc2c900db6552ead821
Carly Reiter <yepodacarly@gmail.com>
2018-09-23 12:40:05 -0700

    Added route helper to routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,24 @@ Rails.application.routes.draw do
   root 'tasks#index'
   # root route says what do I get for '/'
   #localhost:3000 goes to route route
-
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch '/tasks/:id', to: 'tasks#update'
-
+  resources :tasks
   post '/tasks/:id/checkoff', to: 'tasks#checkoff', as: 'checkoff_task'
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  #
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
+  #
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch '/tasks/:id', to: 'tasks#update'
+  #
+  # post '/tasks/:id/checkoff', to: 'tasks#checkoff', as: 'checkoff_task'
+  #
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  # # as: 'delete_task'
 
-  delete '/tasks/:id', to: 'tasks#destroy'
-  # as: 'delete_task'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
