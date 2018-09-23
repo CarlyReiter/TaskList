09deb11897a06c92e250c9499ae61a1abc91f41e
Carly Reiter <yepodacarly@gmail.com>
2018-09-23 13:57:25 -0700

    Cleaning up comments and whitespace.

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,10 @@ Rails.application.routes.draw do
   # root route says what do I get for '/'
   #localhost:3000 goes to route route
   resources :tasks
-  post '/tasks/:id/checkoff', to: 'tasks#checkoff', as: 'checkoff_task'
-  post '/tasks/:id/uncheck', to: 'tasks#uncheck', as: 'uncheck_task'
-  # get '/tasks', to: 'tasks#index', as: 'tasks'
-  #
-  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  # post '/tasks', to: 'tasks#create'
-  #
-  # get '/tasks/:id', to: 'tasks#show', as: 'task'
-  #
-  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  # patch '/tasks/:id', to: 'tasks#update'
-  #
-  # post '/tasks/:id/checkoff', to: 'tasks#checkoff', as: 'checkoff_task'
-  #
-  # delete '/tasks/:id', to: 'tasks#destroy'
-  # # as: 'delete_task'
 
+  post '/tasks/:id/checkoff', to: 'tasks#checkoff', as: 'checkoff_task'
+
+  post '/tasks/:id/uncheck', to: 'tasks#uncheck', as: 'uncheck_task'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
